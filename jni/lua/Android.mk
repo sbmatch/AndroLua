c39cb8746957b72afcaad4a504001b49dea69f05
sbmatch <3207754367@qq.com>
2026-08-01 11:59:24 +0800

    feat(lua): downgrade Lua to 5.4

--- jni/lua/Android.mk
+++ jni/lua/Android.mk
@@ -4,6 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := lua
 LOCAL_SRC_FILES := lapi.c lauxlib.c lbaselib.c lcode.c ldblib.c ldebug.c ldo.c ldump.c lfunc.c lgc.c linit.c liolib.c llex.c lmathlib.c lmem.c loadlib.c lobject.c lopcodes.c loslib.c lparser.c lstate.c lstring.c lstrlib.c ltable.c ltablib.c ltm.c lundump.c lvm.c lzio.c lcorolib.c lctype.c lutf8lib.c
-LOCAL_LDLIBS    := -llog -lm
+LOCAL_LDLIBS    := -llog -lm -ldl  -fvisibility=default
+
+LOCAL_CFLAGS := -DLUA_USE_LINUX -DLUA_USE_POPEN -Wl,-E
 
 include $(BUILD_STATIC_LIBRARY)
