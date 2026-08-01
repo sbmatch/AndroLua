890156f5a160a8d644c5220599a5a4a07dbec128
sbmatch <3207754367@qq.com>
2026-08-02 03:17:34 +0800

    注册native java层崩溃监听器

--- src/main/java/com/sbmatch/androlua/MyApp.java
+++ src/main/java/com/sbmatch/androlua/MyApp.java
@@ -11,6 +11,18 @@ import java.io.File;
 import android.content.Intent;
 import java.io.StringWriter;
 import java.io.PrintWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.FileUtils;
+import xcrash.XCrash;
+import xcrash.ICrashCallback;
+import com.tencent.mmkv.MMKV;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+import java.io.FileInputStream;
 
 public class MyApp extends BaseApp<MyApp> {
     @Override
@@ -18,19 +30,62 @@ public class MyApp extends BaseApp<MyApp> {
         DynamicColors.applyToActivitiesIfAvailable(this);
         MagicHelper.init();
 
+        MMKV.initialize(this);
+        MMKV xCrashkv = MMKV.mmkvWithID("xcrash");
+
+        ICrashCallback callback = new ICrashCallback() {
+            @Override
+            public void onCrash(String logPath, String emergency) {
+                try {
+                    xCrashkv.encode("nativeCrashLog", readFileString(logPath));
+                } catch (IOException e) {
+             
+                }
+                
+                if (emergency != null) {
+                    error(emergency);
+                }
+            }
+        };
+
+        String nativeCrashLog = xCrashkv.decodeString("nativeCrashLog", null);
+        if (nativeCrashLog != null) {
+            openCrashActivity("nativeCrashLog", "上次崩溃日志");
+        }
+
+        XCrash.init(this, new XCrash.InitParameters()
+                .setNativeLogCountMax(1)
+                .setNativeDumpAllThreadsWhiteList(new String[]{"^xcrash\\.sample$", "^Signal Catcher$", "^Jit thread pool$", ".*(R|r)ender.*", ".*Chrome.*"})
+                .setNativeDumpAllThreadsCountMax(10)
+                .setNativeCallback(callback)
+//          .setAnrCheckProcessState(false)
+                // .setAnrRethrow(true)
+                // .setAnrLogCountMax(10)
+                // .setAnrCallback(callback)
+                // .setAnrFastCallback(anrFastCallback)
+                .setPlaceholderSizeKb(512)
+                .setLogDir(getExternalFilesDir("xcrash").toString())
+                // .setLogFileMaintainDelayMs(1000))
+        );
+
         setOnCrashListener(new OnBugReportListener() {
             @Override
             public boolean onCrash(Exception e, final File crashLogFile) {
-
-                Intent intent = new Intent(me, com.sbmatch.androlua.DebugCrashActivity.class);
-                intent.putExtra("crash_log_path", crashLogFile.getAbsolutePath());
-                intent.putExtra("error_stacktrace", getStackTraceString(e));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-
+                // TODO: 请在这里处理异常信息
+                // return true时，会在执行完上述代码后关闭 App，但 return false，则会拦截此错误，App 不会闪退，继续运行
+                xCrashkv.encode("javaCrashLog", getStackTraceString(e));
+                openCrashActivity("javaCrashLog", null);
                 return false;
             }
         });
+    }
+
+    void openCrashActivity(String logKey, String title) {
+        Intent intent = new Intent(me, com.sbmatch.androlua.DebugCrashActivity.class);
+        if (title != null) intent.putExtra("title", title);
+        intent.putExtra("logKey", logKey);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     private String getStackTraceString(Exception e) {
@@ -38,5 +93,16 @@ public class MyApp extends BaseApp<MyApp> {
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
         return sw.toString();
+    }
+
+    String readFileString(String path) throws IOException {
+        try (FileInputStream fis = new FileInputStream(path);
+                FileChannel channel = fis.getChannel()) {
+            long size = channel.size();
+            ByteBuffer buffer = ByteBuffer.allocate((int) size);
+            channel.read(buffer);
+            buffer.flip();
+            return StandardCharsets.UTF_8.decode(buffer).toString();
+        }
     }
 }
