672ccdf356131b375e5fa4995267068ddce31a47
sbmatch <3207754367@qq.com>
2026-08-01 12:11:49 +0800

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 require 'import'
 context = activity:getApplicationContext()
+
+local libDir = activity:getApplicationInfo().nativeLibraryDir
+local dataDir = activity:getApplicationInfo().dataDir
+local filesDir = activity:getFilesDir():getAbsolutePath()
+
+package.cpath = string.format("%s;%s/lib?.so;%s/?.so", package.cpath, libDir, libDir)
+
+package.path = string.format("%s;%s/?.lua;%s/?/init.lua", package.path, filesDir, filesDir)
