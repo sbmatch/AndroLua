dea07fb43b1980646a760b2ab9f9057b89a5c700
sbmatch <3207754367@qq.com>
2026-08-01 12:14:08 +0800

    optize init.lua

---
+++ assets/init.lua
@@ -0,0 +1,28 @@
+require 'import'
+
+import 'org.apache.commons.io.*'
+
+context = activity:getApplicationContext()
+
+local libDir = activity:getApplicationInfo().nativeLibraryDir
+local dataDir = activity:getApplicationInfo().dataDir
+
+filesDir = activity:getFilesDir():getAbsolutePath()
+cacheDir = context:getCacheDir():getAbsolutePath()
+
+local lua_version = _VERSION:match("(%d+%.%d+)")
+
+package.cpath = string.format("%s;%s/lib?.so;%s/?.so;%s/usr/local/lib/lua/%s/?.so", package.cpath, libDir, libDir, filesDir, lua_version)
+
+package.path = string.format("%s;%s/?.lua;%s/?/init.lua", package.path, filesDir, filesDir)
+
+
+function copyAssetToDir(assetName, destFilePath)
+      local inputStream = context:getAssets():open(assetName)
+      local destFile = File(destFilePath .. "/" .. assetName)
+      FileUtils:copyInputStreamToFile(inputStream, destFile)
+end
+
+copyAssetToDir("luasocket.zip", cacheDir)
+
+local success = os.execute("unzip -o " .. cacheDir .. "/luasocket.zip" .. " -d " .. filesDir)
